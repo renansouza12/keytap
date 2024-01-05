5fbb9a79737ac46fa7bca1c613e83af6c808b956
RenanSouza <renansouza0122@gmail.com>
2024-01-05 17:35:42 -0300

    servide removed and products added to catalog component

--- src/app/components/sections/catalog/catalog.component.ts
+++ src/app/components/sections/catalog/catalog.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Products } from 'src/app/models/product.model';
-import { ProductsService } from 'src/app/services/products.service';
+
 
 @Component({
   selector: 'app-catalog',
@@ -9,11 +8,44 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent {
-  products$ = new Observable<Products[]>();
-
-  constructor(private productService: ProductsService){
-    this.products$ = this.productService.getProducts();
-  }
-
+  products:Products[] = [
+    {
+      "id": 1,
+      "name": "Break Fast",
+      "price": 1000,
+      "image": "https://i.pinimg.com/564x/66/d6/29/66d629c9d49d55cb916ba8f734fd3c2c.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Japonese",
+      "price": 985,
+      "image": "https://i.pinimg.com/564x/3a/6d/81/3a6d81dc5ff750a6ab96ceb1ecabfe2f.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Astronaut",
+      "price": 900,
+      "image": "https://i.etsystatic.com/42363382/r/il/ebd300/4852351717/il_794xN.4852351717_7win.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Black Theme",
+      "price": 800,
+      "image": "https://i.pinimg.com/736x/ad/d4/73/add4730d97e5ecf10c563f702c4edb39.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Eva",
+      "price": 500,
+      "image": "https://i.pinimg.com/564x/3a/d5/23/3ad523fe5f61cdeca06a61a92ac81e74.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Forest",
+      "price": 700,
+      "image": "https://i.pinimg.com/736x/11/d4/64/11d4644cc878b8b7c29f43506cc8094c.jpg"
+    }
+  ]
+  
 
 }
